69ed663c2ff56298c5ca0890c0548020bdc78702
tj161588211 <tj161588211@users.noreply.github.com>
2026-04-16 18:15:23 +0000

    fix(wpf): add missing System.Windows.Media namespace for SolidColorBrush

--- wvlan-client/src/WpfClient/ViewModels/MainWindowViewModel.cs
+++ wvlan-client/src/WpfClient/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using WVLAN.Client.Models;
